9d6f1d4d45056cb7ac64d87e1e38b33395ba7842
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2023-12-22 21:13:21 +0300

    Proje dosyası ekle.

---
+++ BootcampWebApiCreate/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootcampWebApiCreate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        public List<string> Get()
+        {
+            return new List<string>
+            {
+                "Ankara",
+                "Erzurum",
+                "İstanbul",
+                "Adana",
+                "İzmir"
+            };
+
+        }
+    }
+
+}
