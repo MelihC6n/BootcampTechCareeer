570df6273a476a991b2d2c5d7cf0da88e4ea61d8
Melih Can Akgüneş <melihcanakgunes@gmail.com>
2023-12-22 21:49:55 +0300

    finish

--- BootcampWebApiCreate/Controllers/ValuesController.cs
+++ BootcampWebApiCreate/Controllers/ValuesController.cs
@@ -7,18 +7,40 @@ namespace BootcampWebApiCreate.Controllers
     [ApiController]
     public class CountriesController : ControllerBase
     {
-        public List<string> Get()
+        [HttpGet]
+        public string[] Get()
         {
-            return new List<string>
-            {
-                "Ankara",
-                "Erzurum",
-                "İstanbul",
-                "Adana",
-                "İzmir"
+            string[] countries = new string[]{
+            "United States", "Canada", "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and/or Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh"
+             };
+
+            return countries;
+        }
+
+        [HttpGet("{id}")]
+        public string Get2(int id)
+        {
+            string[] countries = new string[]{
+            "United States", "Canada", "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and/or Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh"
             };
 
-        }
-    }
 
+
+        if (id < 0 || id >= countries.Length)
+            {
+                return "ID bulunamadı";
+            }
+            else
+            {
+                string result = "";
+                for (int i = 0; i < id; i++)
+                {
+                    result += countries[i]+", ";
+                };
+                return result;
+
+            }
+        }
+
+    } 
 }
